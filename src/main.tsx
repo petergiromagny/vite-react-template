152376e89f2743896f8055323da3dcafb937e39f
Peter <giromagnypeter@gmail.com>
2025-02-10 17:20:04 +0100

    chore: integrate Tailwind CSS and update to version 4.0.6

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,8 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 
 import App from './App'
-import './index.css'
 
-createRoot(!document.getElementById('root') as unknown as HTMLElement).render(
+createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
